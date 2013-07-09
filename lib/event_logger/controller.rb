a0dedd274e2ffb5957770ad9f9d3a9ac5fd1fbff
Ryan Bennick <ryan@320ny.com>
2013-07-08 21:29:28 -0600

    Version bump to 0.1.0

--- lib/event_logger/controller.rb
+++ lib/event_logger/controller.rb
@@ -6,6 +6,23 @@ module EventLogger
 
     protected 
 
+    # <tt>log_event</tt> is used to log any arbiraty event.
+    # It should be called directly in a controller action.
+    # The method takes two options:
+    # 1. event [string] - required
+    # 2. object [object] - optional
+    #
+    # Example:
+    #   
+    #   class UsersController < ApplicationController
+    #     
+    #     def create
+    #       user = User.create(name: 'Bob Vance')
+    #       log_event('user_created', user)
+    #     end
+    #
+    #   end
+    #
     def log_event(event, object=nil)
       log = EventLog.new(event: event, object: object)
       log.save
